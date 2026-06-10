6f987d321e8a26a55efd6dea71bbb0c96ea6038e
Wta00 <christineellise0@gmail.com>
2026-06-10 16:24:45 +0530

    Add WhiteNoise for static files

--- academy/settings.py
+++ academy/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +138,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "academy" / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
